037991f62e85b60d54af016d1a953a0162f6baa7
namadjw12 <jwsuh0703@gmail.com>
2018-07-18 14:04:17 +0900

    make_button

---
+++ First-project/app/src/main/java/org/androidtown/first_project/MainActivity.java
@@ -0,0 +1,20 @@
+package org.androidtown.first_project;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void onButton1Clicked(View v){
+        Toast.makeText(getApplicationContext(), "시작버튼이 눌렸어요.", Toast.LENGTH_SHORT).show();
+    }
+}
